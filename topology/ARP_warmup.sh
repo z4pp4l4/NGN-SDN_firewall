e45bfbf58fd292d95b82fc2fd269deb33c8f7117
Ossyk <ouss.kasraoui03@gmail.com>
2026-01-21 13:45:46 +0100

    warmup cleaning

--- topology/ARP_warmup.sh
+++ topology/ARP_warmup.sh
@@ -4,14 +4,6 @@ echo "[INFO] WARMING UP ARP CACHES..."
 echo "-------------------------------------"
 echo
 
-# INTERNAL HOSTS
-INT_HOSTS=("h1" "h2" "h3")
-
-# EXTERNAL ATTACKERS
-ATTACKERS=("ext1" "ext2" "ext3" "ext4" "ext5" "ext6" "ext7")
-TARGET_IP="192.168.10.1"         # target inside internal subnet
-TARGET_PORT="2020"               # monitored port in firewall
-
 # Pre-populate ARP tables
 echo "[INFO] Warming up ARP caches..."
 for i in 1 2 3; do
